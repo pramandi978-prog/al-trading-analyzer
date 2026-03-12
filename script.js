0a918fb0e89c00bcef6a032025429624d21c7030
pramandi978-prog <pramandi978@gmail.com>
2026-03-12 03:15:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById("result").innerHTML = "Analyzing market...";
 let bullish = Math.floor(Math.random()*40)+50;
 let bearish = 100-bullish;
 setTimeout(()=>{
-document.getElementById("result").innerHTML = '
+document.getElementById("result").innerHTML = `
 market: ${pair} <br><br>
 bullish probability: ${bullish}% <br>
 bearish probability: ${bearish}% <br><br>
